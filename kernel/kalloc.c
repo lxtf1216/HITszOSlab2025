a1d986209b12a06bafdb566cc67b7546f8682b15
3ch0 <2129609136@qq.com>
2025-10-09 19:40:57 +0800

    :zap: improve kalloc

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,6 +9,7 @@
 #include "riscv.h"
 #include "defs.h"
 
+void kfree4cpu(void *pa,int id);
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -18,15 +19,20 @@ struct run {
   struct run *next;
 };
 
-struct {
+struct kmem{
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} ;
+static char kmem_name[16];
+struct kmem kmems[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0;i < NCPU ; ++i) {
+    snprintf(kmem_name , sizeof(kmem_name),"kmem%02d",i);
+    initlock(&(kmems[i].lock),kmem_name);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,8 +41,11 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+  int id = 0;
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+    kfree4cpu(p,id);
+    id = (id + 1) %NCPU;
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -55,11 +64,29 @@ kfree(void *pa)
   memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
+  int id = cpuid();
+  struct kmem *cur_kmem = &(kmems[id]);
+  acquire(&(cur_kmem->lock));
+  r->next = cur_kmem->freelist;
+  cur_kmem->freelist = r;
+  release(&(cur_kmem->lock));
+}
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+void kfree4cpu(void *pa,int id) {
+  struct run *r;
+
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree");
+
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, PGSIZE);
+
+  r = (struct run*)pa;
+  struct kmem *cur_kmem = &(kmems[id]);
+  acquire(&(cur_kmem->lock));
+  r->next = cur_kmem->freelist;
+  cur_kmem->freelist = r;
+  release(&(cur_kmem->lock));
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,15 +95,45 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
+
+  int id = cpuid();
+  struct kmem *cur_kmem = &(kmems[id]);
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  // alloc locally
+  acquire(&(cur_kmem->lock));
+  r = cur_kmem->freelist;
 
-  if(r)
+  if(r) {
+    cur_kmem->freelist = r->next;
+  }
+  release(&(cur_kmem->lock));
+
+  if(r) {
     memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+    return (void*)r;
+  }
+
+  //steal from other CPU
+  for(int i = 1;i< NCPU ; ++i) {
+
+    struct kmem *other_kmem = &(kmems[(id+i)%NCPU]);
+
+    acquire(&(other_kmem->lock)) ;
+
+    r=other_kmem->freelist;
+
+    if(r) {
+      other_kmem->freelist = r->next;
+    }
+
+    release(&(other_kmem->lock));
+
+    if(r) {
+      memset((char*)r, 5, PGSIZE); // fill with junk
+      return (void*)r;
+    }
+  }
+
+  return 0;
 }
